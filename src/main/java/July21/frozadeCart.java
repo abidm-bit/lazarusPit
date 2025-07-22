4a30756da460afb21b1fa7c5731421a1dd4bc485
abidm-bit <abidmres@gmail.com>
2025-07-22 13:18:30 -0400

    enum, class

--- src/main/java/July21/frozadeCart.java
+++ src/main/java/July21/frozadeCart.java
@@ -1,17 +1,36 @@
 package July21;
 
+import java.util.Arrays;
+import java.util.EnumMap;
+
+enum HAAGENDAAZ {
+ CHOCOLATE, ALMOND_CHOCOLATE
+}
+
+enum BENNJERRYS{
+ ICE_CREAM_SANDWICH, STRAWBERRY
+}
+
+enum FROZADE{
+ STRAWBERRY_DAQUIRI,LEMONADE
+}
+
 class frozadeCart {
 
- enum HAAGENDAAZ {
-  CHOCOLATE, ALMOND_CHOCOLATE
+ void iceCream(Enum<?> flavor) {
+  System.out.println("I would like " + flavor.name());
  }
 
- enum BENNJERRYS{
-  ICE_CREAM_SANDWICH, STRAWBERRY
- }
+}
 
- enum FROZADE{
-  STRAWBERRY_DAQUIRI,LEMONADE
+class JavitsCenter {
+
+ public static void main(String[] args) {
+//  System.out.println( HAAGENDAAZ.ALMOND_CHOCOLATE);         // ALMOND_CHOCOLATE
+//  System.out.println(Arrays.toString(HAAGENDAAZ.values())); // [CHOCOLATE, ALMOND_CHOCOLATE]
+  frozadeCart cart = new frozadeCart();
+  cart.iceCream(FROZADE.LEMONADE);
+  cart.iceCream(HAAGENDAAZ.ALMOND_CHOCOLATE);
  }
 
 
