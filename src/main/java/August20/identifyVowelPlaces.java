9c56594235429ac11c99de00427214a245a3fd79
abidm-bit <abidmres@gmail.com>
2025-08-21 00:17:34 -0400

    identify vowel indices

---
+++ src/main/java/August20/identifyVowelPlaces.java
@@ -0,0 +1,46 @@
+package August20;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class identifyVowelPlaces {
+
+
+    // aeiou
+    // String to lower case
+
+    // dexter
+
+//    static void ivp(String input){
+//        input = input.toLowerCase();
+//        HashMap<Character,Integer> answer = new HashMap<>();
+//        for(int i=0;i<input.length();i++){
+//            if(input.charAt(i)=='a'||input.charAt(i)=='e'|| input.charAt(i)=='i'||input.charAt(i)=='o'|| input.charAt(i)=='u'){
+//                answer.put(input.charAt(i),i);
+//            }
+//        }
+//        System.out.println(answer.entrySet());
+//    }
+
+
+    //big o: space O(n) , time O(n)
+    static void ivp(String input){
+        input = input.toLowerCase();
+        ArrayList<Integer> vowelIndices = new ArrayList<>();
+        for(int i=0;i<input.length();i++){
+            if(input.charAt(i)=='a'||input.charAt(i)=='e'|| input.charAt(i)=='i'||input.charAt(i)=='o'|| input.charAt(i)=='u'){
+                vowelIndices.add(i);
+            }
+        }
+        System.out.println(vowelIndices);
+    }
+
+    public static void main(String[] args) {
+
+        ivp("DEXTER");
+        ivp("confidentiality");
+        ivp("integrity");
+        ivp("availability");
+        ivp("RADAR");
+    }
+}
