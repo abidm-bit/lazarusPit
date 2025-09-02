4da8d485e25228250b66b85f23a124944864befd
abidm-bit <abidmres@gmail.com>
2025-09-02 15:33:05 -0400

    sequential to threads

---
+++ src/main/java/September2v1/Printers.java
@@ -0,0 +1,36 @@
+package September2v1;
+
+public class Printers {
+    public static void main(String[] args) {
+
+        System.out.println("Printing in fast draft, so this won't be in order");
+    for(int i=1;i<=3;i++){
+        for(int e =1 ; e<=7; e++){
+            Printer printer = new Printer(i,e);
+            printer.start();
+        }
+    }
+
+
+    }
+}
+
+class Printer extends Thread{
+
+    private int printerNumber;
+    private int pagesToPrint;
+
+    public Printer(int printerNumber,int pagesToPrint){
+        this.printerNumber=printerNumber;
+        this.pagesToPrint=pagesToPrint;
+    }
+
+    @Override
+    public void run() {
+        for(int pages=1;pages<=pagesToPrint;pages++){
+        System.out.println("printer # "+this.printerNumber + " is printing page # " + pages);}
+    }
+
+}
+
+
---
+++ src/main/java/September2v1/Sequential.java
@@ -0,0 +1,22 @@
+package September2v1;
+
+public class Sequential {
+
+    private static void Printer1(){
+        for(int i=1;i<=5;i++){
+            System.out.println("Printer 1 is printing page " + i);
+        }
+    }
+
+    private static void Printer2(){
+        for(int e=1;e<=3;e++){
+            System.out.println("Printer 2 is printing page " + e);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Printer1();
+        Printer2();
+    }
+}
