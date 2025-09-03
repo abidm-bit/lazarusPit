236cd1adfd3d4e019cd7227658e6d89b1fe36d82
abidm-bit <abidmres@gmail.com>
2025-09-03 19:23:53 -0400

    Dynamic: Lists, Stack & Queue

--- src/main/java/September3/v3/MoarDataStructure.java
+++ src/main/java/September3/v3/MoarDataStructure.java
@@ -1,12 +1,18 @@
 package September3.v3;
 
-import java.util.Arrays;
+import java.util.*;
 
 public class MoarDataStructure {
 
     public static void main(String[] args) {
-    Linear linear = new Linear();
-    linear.fixedMultiItem(); // Arrays
+        Linear linear = new Linear();
+        //    linear.fixedMultiItem();                  // Array
+
+        Dynamic dynamic = new Dynamic();
+        //        dynamic.ListArray();                  // ArrayList
+        //            dynamic.ListLinked();             // LinkedList
+        //                dynamic.BakeACake();          // Stack
+        //                      dynamic.gameplay();     // Queue
 
     }
 }
@@ -21,7 +27,9 @@ class Linear{
                 O(1) to select an element by its index  arrName[index]
                 .length() = # of elements in an array
                 0(1) to replace an element by its index arrName[index] = newValue;
+                O(n) to iterate thru, depending on number of elements
                 O(n) to duplicate an array: create an array and iterate thru
+                    primitive, Wrapper
 
          */
 
@@ -68,7 +76,108 @@ class Linear{
 
     }
 
+}
 
+class Dynamic{
+
+    void ListArray(){
+
+        /*
+                ArrayLists are dynamic, the size isn't fixed
+                Wrapper
+                O(1) access elements by index
+                .size()
+                O(n) to iterate thru, depending on number of elements
+
+        */
+
+        ArrayList<String> fonts = new ArrayList<>();
+        fonts.add("Comic Sans"); fonts.add("Verdana"); fonts.add("Times New Roman");
+        System.out.println(fonts);                  // print as an array
+        for(String f:fonts){
+            System.out.print(f + ", ");              // print the List items in one line
+        }
+        System.out.println();
+
+        ArrayList<Integer> numbers = new ArrayList<>(List.of(3,6,9,9,8,6,3));
+        System.out.println(numbers.size());                                 //  7 elements
+        numbers.forEach(numb-> System.out.print(numb+", "));         // print using a lambda
+
+
+    }
+
+    void ListLinked(){
+        /*
+            LinkedLists take up more storage compared to ArrayLists
+            but have pointers to follow elements before & after
+            O(1) for inserting or deleting elements to the front or end
+            O(1) to access the first & last element
+            O(n) to iterate thru elements
+        */
+
+        LinkedList<String> home = new LinkedList<>();
+        home.add("standing desk"); home.add("carpet"); home.add("bookcase");
+        System.out.println(home.getFirst()); // get the first element: standing desk
+        System.out.println(home.getLast()); // get the last element: bookcase
+        home.addLast("sofa");
+        home.addFirst("gaming chair");
+        for(String furn:home){
+            System.out.print(furn + " ");
+        }
+        System.out.println();
+        System.out.println(home.size() + " elements"); // 5 elements after adding new elements
+    }
+
+    void BakeACake(){
+        /*
+        O(n) to iterate thru
+        O(1) to access the top item, porque LIFO
+        O(1) to add to a stack .add / .push
+         O(1) to remove .pop
+        */
+
+        Stack<String> games = new Stack<>();
+        games.add("GTA V");
+        games.add("UFC");
+        games.add("Sifu");
+        System.out.println(games.peek()); // first element on top , expect Sifu, being the latest game
+        for(String g:games){
+            System.out.print(g + ", ");
+        }
+
+    }
+
+    void gameplay(){
+        /*
+            Queues are FIFO, think of it as a procedure / line
+            O(n) to iterate thru
+            O(1) to add
+        */
+        Queue<String> codmw4 = new LinkedList<>();
+        codmw4.offer("Gun Range");
+        codmw4.offer("Ship");
+        codmw4.offer("Invasion");
+        codmw4.offer("Chernobyl");
+        Iterator<String> missions = codmw4.iterator();
+        while(missions.hasNext()){
+            System.out.println(missions.next());
+        }
+        System.out.println();
+        Queue<Integer> eyeglass = new ArrayDeque<>();
+        eyeglass.add(3971);
+        eyeglass.offer(3972);
+        System.out.println("3971 has to leave");
+        eyeglass.remove();
+        eyeglass.add(3973);
+        eyeglass.add(3974);
+        System.out.println("3974 was called three times, next customer");
+        eyeglass.offer(3975);
+        for(Integer li:eyeglass){
+            System.out.println("Customer "+ li);
+        }
+
+    }
 
 
 }
+
