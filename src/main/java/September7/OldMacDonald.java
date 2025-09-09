6283b97c714efc7a297042fe38c1d06f7c695d00
abidm-bit <abidmres@gmail.com>
2025-09-09 15:42:52 -0400

    m2 is not good because of a 2nd occurrence won't be recorded by a hashmap

--- src/main/java/September7/OldMacDonald.java
+++ src/main/java/September7/OldMacDonald.java
@@ -16,24 +16,24 @@ public class OldMacDonald {
         System.out.println(answer);
     }
 
-    static void vowelIndicesM2(String input){
-        input=input.toLowerCase();
-        HashMap<Character,Integer> answer = new HashMap<>();
-        for(int i =0;i<input.length();i++){
-            if(input.charAt(i)=='a' ||input.charAt(i)=='e'|| input.charAt(i)=='i' || input.charAt(i)=='o' || input.charAt(i)=='u'){
-                answer.put(input.charAt(i),i);
-            }
-        }
-        System.out.println(answer.entrySet());
-    }
+    // not good because of the 2nd occurrence
+//    static void vowelIndicesM2(String input){
+//        input=input.toLowerCase();
+//        HashMap<Character,Integer> answer = new HashMap<>();
+//        for(int i =0;i<input.length();i++){
+//            if(input.charAt(i)=='a' ||input.charAt(i)=='e'|| input.charAt(i)=='i' || input.charAt(i)=='o' || input.charAt(i)=='u'){
+//                answer.put(input.charAt(i),i);
+//            }
+//        }
+//        System.out.println(answer.entrySet());
+//    }
 
 
     public static void main(String[] args) {
         vowelIndicesM1("wire");
-        vowelIndicesM2("wire");
-        System.out.println();
 
-        vowelIndicesM2("old macdonald had a farm");
+        vowelIndicesM1("stackheap");
+        vowelIndicesM1("old macdonald had a farm");
     }
 
 }
