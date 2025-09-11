acb4a7e727b27d73cfaeb4278e8fc73573bc81f9
abidm-bit <abidmres@gmail.com>
2025-09-11 10:12:40 -0400

    overloading constructors

--- src/main/java/September11/Wednesday.java
+++ src/main/java/September11/Wednesday.java
@@ -5,7 +5,8 @@ import java.util.Objects;
 
 enum draw{
     LINE_ART,
-    GRAFFITI
+    GRAFFITI,
+    GRAPHIC_DESIGN
 }
 enum mediums{
     WALLS,
@@ -24,8 +25,11 @@ class Art{
     }
 
     Art(boolean digitalArt,String artType){
-       if(digitalArt && !Objects.equals(artType, draw.GRAFFITI.name())){ System.out.println("I'm drawing " + artType + " in " + mediums.PIXELMATOR.name());}
-        else {
+       if(digitalArt && Objects.equals(artType, draw.GRAFFITI.name()))
+       { System.out.println("I'm drawing " + artType + " in " + mediums.PIXELMATOR.name());}
+       else if (digitalArt && artType.equals(draw.GRAPHIC_DESIGN.name())) {
+           System.out.println("I'm designing graphics using " + mediums.PIXELMATOR.name());
+       } else {
            System.out.println("I'm writing " + artType + " in " +  mediums.PIXELMATOR.name());
        }
     }
@@ -50,6 +54,9 @@ public class Wednesday {
 
             new Art(true,draw.GRAFFITI.name());
 
+            System.out.println();
+
+            new Art(true,draw.GRAPHIC_DESIGN.name());
     }
 
 }
