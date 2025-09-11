6d68c60877b354577c30d718481456c716237d89
abidm-bit <abidmres@gmail.com>
2025-09-11 10:08:33 -0400

    overloading constructors

---
+++ src/main/java/September11/Wednesday.java
@@ -0,0 +1,55 @@
+package September11;
+
+
+import java.util.Objects;
+
+enum draw{
+    LINE_ART,
+    GRAFFITI
+}
+enum mediums{
+    WALLS,
+    PAPER,
+    PIXELMATOR
+}
+class Art{
+
+    Art(String artType,String medium){
+        if(artType.equals(draw.GRAFFITI.name())){
+            System.out.println("I'm writing " + artType + " on " + medium);
+        }
+        else {
+            System.out.println("I'm drawing " + artType + " on " + medium);
+        }
+    }
+
+    Art(boolean digitalArt,String artType){
+       if(digitalArt && !Objects.equals(artType, draw.GRAFFITI.name())){ System.out.println("I'm drawing " + artType + " in " + mediums.PIXELMATOR.name());}
+        else {
+           System.out.println("I'm writing " + artType + " in " +  mediums.PIXELMATOR.name());
+       }
+    }
+
+}
+
+public class Wednesday {
+
+    public static void main(String[] args) {
+
+            new Art(draw.GRAFFITI.name(), mediums.WALLS.name());
+
+            System.out.println();
+
+            new Art(draw.LINE_ART.name(), mediums.PAPER.name());
+
+            System.out.println();
+
+            new Art(true,draw.LINE_ART.name());
+
+            System.out.println();
+
+            new Art(true,draw.GRAFFITI.name());
+
+    }
+
+}
